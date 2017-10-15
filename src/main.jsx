4803da81498a812f70a419be082003f8f9c7a1b0
Michael Grudkowski <mgrudkowski@gmail.com>
2017-10-15 15:45:34 -0400

    viewport

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 "use strict"
-
+ 
 import ReactDOM from 'react-dom';
 import React from 'react';
-//import SymbolGridTest from './symbolGridTest/SymbolGridTest';
-import ViewportTest from './viewPort/ViewportTest';
+import ViewportTest from './viewportTest/ViewportTest';
 
-console.log("rendering application");
 ReactDOM.render(
-//  <Chart>Testing</Chart>,
   <ViewportTest/>,
   document.getElementById('app'));
 
--- src/viewPortTest/ViewportTest.jsx
+++ src/viewPortTest/ViewportTest.jsx
@@ -2,9 +2,83 @@
 
 import React from 'react';
 import { AutoSizer } from 'react-virtualized';
+import tooltip from 'react-toolbox/lib/tooltip';
+
 import styles from './ViewportTest.css';
 
+export default class ViewportTest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    console.log("ViewportTest::ctor");
+  }
 
-export default () => {
-  <div>Testing</div>
+  onClick() {
+    console.log("onClick");
+  }
+
+  render() {
+  
+    return (
+      <div style={{display: 'flex', minWidth: '100', flexWrap: 'wrap', flexDirection: 'row-reverse' }}>
+        <div style={{flex: '1 1 auto',  minWidth: '100%', backgroundColor: 'blue', fontFamily: 'Roboto' }}>
+          Testing.
+        </div> 
+	<svg style={{flex: '1 1 auto', minWidth: '100'}}>
+	  <rect width="50" height="30" x="0" fill="red" />
+	  <rect width="30" height="30" x="0" fill="limegreen"/>
+	  <text x="8" y="22" fontFamily="Roboto" fontSize="15">
+	    AAPL
+	  </text>
+	  <rect  width="50" height="30" x="0" style={{fill: 'blue', opacity: 0, cursor: 'pointer'}} onClick={this.onClick}>
+	    <title style={{color: 'yellow'}}>Hello <br/>There</title>
+	  </rect>
+	  <rect width="50" height="30" x="52" fill="red"/>
+          <text x="60" y="22" fontFamily="Roboto" fontSize="15">
+            AMZN
+          </text>
+ 
+          <rect width="50" height="30" x="104" fill="red"/>
+	  <text x="112" y="22" fontFamily="Roboto" fontSize="15">
+            BIDU
+          </text>
+ 
+          <rect width="50" height="30" x="156" fill="red"/>
+	  <rect width="50" height="30" x="208" fill="red"/>
+	  <rect width="50" height="30" x="260" fill="red"/>
+          <rect width="50" height="30" y="32" x="0" fill="red"/>
+          <rect width="50" height="30" y="32" x="52" fill="red"/>
+          <rect width="50" height="30" y="32" x="104" fill="red"/>
+          <rect width="50" height="30" y="32" x="156" fill="red"/>
+          <rect width="50" height="30" y="32" x="208" fill="red"/>
+          <rect width="50" height="30" y="32" x="260" fill="red"/>
+	  <rect width="50" height="30" y="132" x="0" fill="red"/>
+          <rect width="50" height="30" y="132" x="52" fill="red"/>
+          <rect width="50" height="30" y="132" x="104" fill="red"/>
+          <rect width="50" height="30" y="132" x="156" fill="red"/>
+          <rect width="50" height="30" y="132" x="208" fill="red"/>
+          <rect width="50" height="30" y="132" x="260" fill="red"/>
+ 	  <rect width="50" height="30" y="232" x="0" fill="red"/>
+          <rect width="50" height="30" y="232" x="52" fill="red"/>
+          <rect width="50" height="30" y="232" x="104" fill="red"/>
+          <rect width="50" height="30" y="232" x="156" fill="red"/>
+          <rect width="50" height="30" y="232" x="208" fill="red"/>
+          <rect width="50" height="30" y="232" x="260" fill="red"/>
+ 	  <rect width="50" height="30" y="332" x="0" fill="red"/>
+          <rect width="50" height="30" y="332" x="52" fill="red"/>
+          <rect width="50" height="30" y="332" x="104" fill="red"/>
+          <rect width="50" height="30" y="332" x="156" fill="red"/>
+          <rect width="50" height="30" y="332" x="208" fill="red"/>
+          <rect width="50" height="30" y="332" x="260" fill="red"/>
+ 
+	</svg>
+      </div>
+    );
+  }
 }
+
+/*
+export default () => {
+  return <div>Testing</div>
+}
+*/
